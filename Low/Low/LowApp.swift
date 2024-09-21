3ababb4a671864b5948ee98cb9550c6e96b3df9f
ROUSE-prog <stevenr2049@gmail.com>
2024-09-20 23:36:22 -0400

    Initial Xcode project setup

---
+++ Low/Low/LowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LowApp.swift
+//  Low
+//
+//  Created by Main on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
